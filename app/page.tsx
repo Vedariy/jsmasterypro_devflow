cd08fd491911c8c483c2708f304bda9507afe76d
viktor_miriuschenko <vedariy@gmail.com>
2024-12-12 03:03:04 +0300

    eslint and prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
-
-const Home=() =>{
+const Home = () => {
   return (
-    <h1 className="text-3xl font-bold underline">Welcome to the world of Nextjs</h1>
-  );
+    <h1 className="text-3xl text-white font-black underline">
+      Welcome to the world of Nextjs
+    </h1>
+  )
 }
-
 export default Home
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,62 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "standard",
+    "plugin:tailwindcss/recommended",
+    "prettier"
+  ),
+  {
+    plugins: ["import"],
+    rules: {
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            ["parent", "sibling"],
+            "index",
+            "object",
+          ],
+          "newlines-between": "always",
+          pathGroups: [
+            {
+              pattern: "@app/**",
+              group: "external",
+              position: "after",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["builtin"],
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+    ignorePatterns: ["components/ui/**"],
+    overrides: [
+      {
+        files: ["*.ts", "*.tsx"],
+        rules: {
+          "no-undef": "off",
+        },
+      },
+    ],
+  },
 ];
 
 export default eslintConfig;
